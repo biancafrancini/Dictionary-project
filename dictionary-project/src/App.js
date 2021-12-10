13ecb0a6ae446c1cfd12750521fa8e9e118eaee3
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2021-12-10 22:17:26 +0000

    Setting up

---
+++ dictionary-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="Dictionary-logo img-fluid" alt="logo" />
+        <p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
