32fb9122e13906907db58d4520ca9a6dccd31442
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2021-12-12 11:38:16 +0000

    Added definitions and meanings
    using useState and map

--- dictionary-project/src/Dictionary.js
+++ dictionary-project/src/Dictionary.js
@@ -1,11 +1,15 @@
 import React , { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary(){
-    let [keyWord , setKeyWord] = useState(null);
+    let [keyWord , setKeyWord] = useState("");
+    let [results, setResults] = useState(null);
+
  function handleResponse(response){
      console.log(response.data[0]);
+     setResults(response.data[0]);
  }    
 
     function search(event) {
@@ -24,6 +28,7 @@ export default function Dictionary(){
             <form onSubmit={search}>
                 <input type="search" autoFocus={true} placeholder="Type a word.." onChange={handleKeyWordChange} />
             </form>
+            <Results results={results} />
         </div>
     );
 }
