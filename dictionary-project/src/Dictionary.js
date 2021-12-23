5f24956f2833e901311d8d175ae9e374b4b04d19
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2021-12-23 23:28:40 +0100

    Added photos

--- dictionary-project/src/Dictionary.js
+++ dictionary-project/src/Dictionary.js
@@ -1,16 +1,21 @@
 import React , { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function Dictionary(){
     let [keyWord , setKeyWord] = useState("");
     let [results, setResults] = useState(null);
+    let[photo, setPhoto] = useState(null);
 
  function handleResponse(response){
-     console.log(response.data[0]);
      setResults(response.data[0]);
  }    
+
+ function handlePexelsResponse(response){
+    setPhoto(response.data.photos);
+ }
 
  function handleSubmit(event){
     event.preventDefault();
@@ -19,6 +24,11 @@ export default function Dictionary(){
     function search(keyword) {
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = "563492ad6f917000010000015117a13c13c34f409dd1ce2d42445508";
+    let pexelsApiUrl= `https://api.pexels.com/v1/search?query=${keyWord}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
     
 
@@ -34,6 +44,7 @@ export default function Dictionary(){
             <br />
  
             <Results results={results} />
+            <Photos photos={photo} />
         </div>
     );
 }
